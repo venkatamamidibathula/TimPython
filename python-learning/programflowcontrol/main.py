787b1efe6bf65374245d2045a43cb28e8cdf94b8
venkata.mamidibathula@outlook.com <venkata.mamidibathula@outlook.com>
2023-11-03 08:34:28 -0500

    ADDED ALBUM

---
+++ python-learning/programflowcontrol/main.py
@@ -0,0 +1,7 @@
+### This program to print multiplication tables in Python ####
+for i in range(1,13):
+    for j in range(1,13):
+        print("{0} times {1} is {2}".format(j,i,j*i))
+    print("================================================")
+
+
